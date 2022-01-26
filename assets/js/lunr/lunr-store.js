757640f00bd99342537994af7daab04609eac309
Tschokko <Tschokko@users.noreply.github.com>
2022-01-26 19:57:56 +0000

    jekyll build from Action 0a368f127f6e285e130e54c4e795f0c54905198d

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Aller Anfang ist schwer",
+        "excerpt":"Nüscht wie Ärjer und Freude! Genau mit dieser Aussage nahm dieses Projekt seinen Verlauf. Ein Urgestein der Modelleisenbahn tat seinem Unmut kund und adressierte direkt meine laufenden Spur 0 Entwicklungen. Bis dato entwickelte ich meine Sachen für mich selbst und teilte lediglich meine Fortschritte, ohne mir großartig Gedanken darüber zu...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2021/12/27/aller-anfang-ist-schwer.html",
+        "teaser": null
+      },{
+        "title": "Das war wohl nix!",
+        "excerpt":"Hallo! So schnell kann es gehen. Ich werde die Seite voraussichtlich wieder offline nehmen. Quasi, alle Arbeit für die Katz. Eine prominente Spur 0 Webseite hat nun einen Download Bereich eröffnet, sogar mit Open Source Lizenzen! Tja, dagegen kann ich nicht anstinken. Ebenso wenig habe ich großes Interesse daran, das...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2022/01/25/das-war-wohl-nix.html",
+        "teaser": null
+      },{
+        "title": "Spur 0 Gleise",
+        "excerpt":"Info: Die aktuellen Konstruktionsdateien sind im GitHub Repository github.com/projekt-null/Spur_0_Gleise hinterlegt. Um das Repository lokal als Gast zu clonen, führe folgendes git-Kommando aus: git clone https://github.com/projekt-null/Spur_0_Gleise.git Vorwort Es ist mir ein persönliches Anliegen auf die Gleise und Weichen von Michael Weinert hinzuweisen. Im Rahmen einer FREMO Initiative wurden maßstäbliche Gleise und...","categories": [],
+        "tags": [],
+        "url": "/projekte/spur-0-gleise/einfuehrung/",
+        "teaser": null
+      },{
+        "title": "Spur 0 Gleise",
+        "excerpt":"Info: Die aktuellen Konstruktionsdateien sind im GitHub Repository github.com/projekt-null/Spur_0_Gleise hinterlegt. Um das Repository lokal als Gast zu clonen, führe folgendes git-Kommando aus: git clone https://github.com/projekt-null/Spur_0_Gleise.git Regelspur Gleise Holzschwellen Zu Grunde liegen: Spurweite von 32mm Einsatz von K-Modul-System Rippenplatten Holzschwellen in den Abmessungen von 58x5,8mm (entspricht 2,6m im Vorbild) Info: Die...","categories": [],
+        "tags": [],
+        "url": "/projekte/spur-0-gleise/regelspur-gleise-und-schablonen/",
+        "teaser": null
+      },{
+        "title": "Spur 0 Gleise",
+        "excerpt":"Info: Die aktuellen Konstruktionsdateien sind im GitHub Repository github.com/projekt-null/Spur_0_Gleise hinterlegt. Um das Repository lokal als Gast zu clonen, führe folgendes git-Kommando aus: git clone https://github.com/projekt-null/Spur_0_Gleise.git Regelspur Weiche EW 100 1:6,6 Achtung: Die Konstruktionsdateien zur Weiche haben nicht den Weg in das GitHub Repository gefunden. Die Weiche entspricht der EW100 von...","categories": [],
+        "tags": [],
+        "url": "/projekte/spur-0-gleise/regelspur-ew-100-1-6-6/",
+        "teaser": null
+      },{
+        "title": "Spur 0 Module",
+        "excerpt":"Info: Die aktuellen Konstruktionsdateien sind im GitHub Repository github.com/projekt-null/Spur_0_Module hinterlegt. Um das Repository lokal als Gast zu clonen, führe folgendes git-Kommando aus: git clone https://github.com/projekt-null/Spur_0_Module.git Kopfstücke Kopfstücke für den Modulbau sind in der Regel genormt. Die entsprechenden Abmessungen können den jeweiligen Dokumenten entnommen werden, z.B. FREMO. Zweck dieses Bereichs ist...","categories": [],
+        "tags": [],
+        "url": "/projekte/spur-0-module/kopfstuecke/",
+        "teaser": null
+      }]
