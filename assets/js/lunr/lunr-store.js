e1ca6156bb09ee740b4c274aa0214c6ea70a1e03
Tschokko <Tschokko@users.noreply.github.com>
2023-10-02 20:03:45 +0000

    jekyll build from Action 256eb4ee1cfd08f0d8099ea65bff2adaa7289f53

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Das ist ein Test,",
+        "excerpt":"denn der Mist muss weg. ;-) Mal sehen ob die Pipeline noch tut…   ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2023/10/02/test.html",
+        "teaser": null
+      },{
+        "title": "Spur 0 Gleise",
+        "excerpt":"Info: Die aktuellen Konstruktionsdateien sind im GitHub Repository github.com/projekt-null/Spur_0_Gleise hinterlegt. Um das Repository lokal als Gast zu clonen, führe folgendes git-Kommando aus: git clone https://github.com/projekt-null/Spur_0_Gleise.git Vorwort Es ist mir ein persönliches Anliegen auf die Gleise und Weichen von Michael Weinert hinzuweisen. Im Rahmen einer FREMO Initiative wurden maßstäbliche Gleise und...","categories": [],
+        "tags": [],
+        "url": "/projekte/spur-0-gleise/einfuehrung/",
+        "teaser": null
+      },{
+        "title": "Spur 0 Gleise",
+        "excerpt":"Info: Die aktuellen Konstruktionsdateien sind im GitHub Repository github.com/projekt-null/Spur_0_Gleise hinterlegt. Um das Repository lokal als Gast zu clonen, führe folgendes git-Kommando aus: git clone https://github.com/projekt-null/Spur_0_Gleise.git Regelspur Gleise Holzschwellen Zu Grunde liegen: Spurweite von 32mm Einsatz von K-Modul-System Rippenplatten Holzschwellen in den Abmessungen von 58x5,8mm (entspricht 2,6m im Vorbild) Info: Die...","categories": [],
+        "tags": [],
+        "url": "/projekte/spur-0-gleise/regelspur-gleise-und-schablonen/",
+        "teaser": null
+      },{
+        "title": "Spur 0 Gleise",
+        "excerpt":"Info: Die aktuellen Konstruktionsdateien sind im GitHub Repository github.com/projekt-null/Spur_0_Gleise hinterlegt. Um das Repository lokal als Gast zu clonen, führe folgendes git-Kommando aus: git clone https://github.com/projekt-null/Spur_0_Gleise.git Regelspur Weiche EW 100 1:6,6 Achtung: Die Konstruktionsdateien zur Weiche haben nicht den Weg in das GitHub Repository gefunden. Die Weiche entspricht der EW100 von...","categories": [],
+        "tags": [],
+        "url": "/projekte/spur-0-gleise/regelspur-ew-100-1-6-6/",
+        "teaser": null
+      },{
+        "title": "Spur 0 Module",
+        "excerpt":"Info: Die aktuellen Konstruktionsdateien sind im GitHub Repository github.com/projekt-null/Spur_0_Module hinterlegt. Um das Repository lokal als Gast zu clonen, führe folgendes git-Kommando aus: git clone https://github.com/projekt-null/Spur_0_Module.git Kopfstücke Kopfstücke für den Modulbau sind in der Regel genormt. Die entsprechenden Abmessungen können den jeweiligen Dokumenten entnommen werden, z.B. FREMO. Zweck dieses Bereichs ist...","categories": [],
+        "tags": [],
+        "url": "/projekte/spur-0-module/kopfstuecke/",
+        "teaser": null
+      }]
